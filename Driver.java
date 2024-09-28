9a7426e82cdd4277f883284d80214b3b192aa7ad
Lucy Lin <lucylin6699@gmail.com>
2024-09-27 20:33:29 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,20 +1,120 @@
+import java.io.File;
+
 public class Driver {
     public static void main(String [] args) {
         Polynomial p = new Polynomial();
-        System.out.println(p.evaluate(3));
+        System.out.println(p.evaluate(3)); // expected output = 0.0
 
-        double [] c1 = {6,0,0,5};
-        Polynomial p1 = new Polynomial(c1);
+        double [] c1 = {6,5}; // 6 + 5x^3
+        int [] e1 = {0,3};
+        Polynomial p1 = new Polynomial(c1, e1);
+        System.out.println("polynomial p1: coe = [");
+        for(double c : p1.coe) {
+            System.out.println(c + " ");
+        } 
+        System.out.println("] ");
+        System.out.println("exp = [");
+        for(int e : p1.exp) {
+            System.out.println(e + " ");
+        } 
 
-        double [] c2 = {0,-2,0,0,-9};
-        Polynomial p2 = new Polynomial(c2);
+        double [] c2 = {-9,-2}; // -9x^4 - 2x
+        int [] e2 = {4,1};
+        Polynomial p2 = new Polynomial(c2, e2);
+        System.out.println("polynomial p2: coe = [");
+        for(double c : p2.coe) {
+            System.out.println(c + " ");
+        } 
+        System.out.println("] ");
+        System.out.println("exp = [");
+        for(int e : p2.exp) {
+            System.out.println(e + " ");
+        } 
 
-        Polynomial s = p1.add(p2);
-        System.out.println("s(0.1) = " + s.evaluate(0.1));
+        double [] c3 = {-6,-2}; // -6-2x
+        int [] e3 = {0,1};
+        Polynomial p3 = new Polynomial(c3, e3);
+        System.out.println("polynomial p3: coe = [");
+        for(double c : p3.coe) {
+            System.out.println(c + " ");
+        } 
+        System.out.println("] ");
+        System.out.println("exp = [");
+        for(int e : p3.exp) {
+            System.out.println(e + " ");
+        } 
+
+
+        Polynomial s = p1.add(p2); // expected = 6 + 5x^3 - 9x^4 - 2x
+        System.out.println("polynomial s: coe = [");
+        for(double c : s.coe) {
+            System.out.println(c + " ");
+        } 
+        System.out.println("] ");
+        System.out.println("exp = [");
+        for(int e : s.exp) {
+            System.out.println(e + " ");
+        } 
+        System.out.println("s(0.1) = " + s.evaluate(0.1)); // expected output = 5.8041
+
+        Polynomial s2 = p1.add(p3); // expected = 5x^3 - 2x
+        System.out.println("polynomial s2: coe = [");
+        for(double c : s2.coe) {
+            System.out.println(c + " ");
+        } 
+        System.out.println("] ");
+        System.out.println("exp = [");
+        for(int e : s2.exp) {
+            System.out.println(e + " ");
+        } 
+        System.out.println("s2(0.1) = " + s2.evaluate(0.1)); // expected output = -0.195
+        
+        Polynomial m = p1.multiply(p2); // expected = -45x^7 - 12x - 64x^4
+        System.out.println("polynomial m: coe = [");
+        for(double c : m.coe) {
+            System.out.println(c + " ");
+        } 
+        System.out.println("] ");
+        System.out.println("exp = [");
+        for(int e : m.exp) {
+            System.out.println(e + " ");
+        } 
+        System.out.println("m(0.1) = " + m.evaluate(0.1)); // expected output = -1.2064045 
         
         if(s.hasRoot(1))
             System.out.println("1 is a root of s");
         else
             System.out.println("1 is not a root of s");
+
+        p1.saveToFile("p1.txt");
+        p2.saveToFile("p2.txt");
+        p3.saveToFile("p3.txt");
+        s.saveToFile("s.txt");
+        s2.saveToFile("s2.txt");
+        m.saveToFile("m.txt");
+
+        File file1 = new File("L:\\Lucy\\UTSC\\4. Fall 2024 courses\\CSCB07\\Lab 2\\b07lab1\\new1.txt");
+        Polynomial new1 = new Polynomial(file1);
+        System.out.println("polynomial new1: coe = [");
+        for(double c : new1.coe) {
+            System.out.println(c + " ");
+        } 
+        System.out.println("] ");
+        System.out.println("exp = [");
+        for(int e : new1.exp) {
+            System.out.println(e + " ");
+        } 
+
+        File file2 = new File("L:\\Lucy\\UTSC\\4. Fall 2024 courses\\CSCB07\\Lab 2\\b07lab1\\new2.txt");
+        Polynomial new2 = new Polynomial(file2);
+        System.out.println("polynomial new2: coe = [");
+        for(double c : new2.coe) {
+            System.out.println(c + " ");
+        } 
+        System.out.println("] ");
+        System.out.println("exp = [");
+        for(int e : new2.exp) {
+            System.out.println(e + " ");
+        } 
     }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,59 +1,165 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.io.PrintStream;
+
 public class Polynomial {
 	//fields
 	double[] coe;
+	int[] exp;
 	
-	//constructors
+	//constructor 1: no argument
 	public Polynomial() {
-		coe = new double[1];
-		coe[0] = 0;
+		coe = null;
+		exp = null;
+	}
+
+	//constructor 2: take in one array of coefficients and one array of exponents
+	public Polynomial(double [] coefficient, int [] exponent) { 
+		coe = coefficient;
+		exp = exponent;
 	}
 	
-	public Polynomial(double [] a) { 
-		coe = new double[a.length];
-		for (int i = 0; i < a.length; i++)
-			coe[i] = a[i];
+	//constructor 3: take in one argument of type File
+	public Polynomial(File file) {
+		try (// create a scanner for reading the file
+		Scanner scanner = new Scanner(file)) {
+			// read and store the first line of the file
+			String polynomial = scanner.nextLine();
+			// split the string using "+" and "-" and the lookahead assertion to preserve the sign of the coefficient
+			String[] terms = polynomial.split("((?=[+-]))");
+			// initialize the two arrays
+			coe = new double[terms.length];
+			exp = new int[terms.length];
+
+			for (int i = 0; i < terms.length; i++) {
+				// split each string again, to obtain the coefficient at index 0
+				// and the exponent, if any, at index 1
+				String[] values = terms[i].split("x", -1);
+
+				// first, check if values is empty, if so, it means that the term is "x"
+				if(values.length == 0) {
+					coe[i] = 1.0;
+					exp[i] = 1;
+				}
+				// make sure that the coefficient is nonzero
+				else {
+					// first, handle the coefficient (value[0])
+					// if it's + or "" (if the x is the first letter in the string), it means the coe is 1
+					// note to self: in Java, .equals compare the content of the object (while == compare the reference)
+					if(values[0].equals("+") || values[0] == "") {
+						coe[i] = 1.0;
+					}
+					// if it's -, it means the coe is -1
+					else if(values[0].equals("-")) {
+						coe[i] = -1.0;
+					}
+					// otherwise, use parsedouble
+					else {
+						// note to self: parseDouble is able to convert "+5" to 5.0 correctly
+						coe[i] = Double.parseDouble(values[0]); 
+					}
+					
+					// now handle the exponents
+					// if the values array has length 1, it means we only have the coefficient part, so exponent is 0
+					if (values.length == 1) {
+						exp[i] = 0;
+					}
+					// if it's "", it means that there's no number after x, meaning x has power of 1
+					else if (values[1].equals("")) {
+						exp[i] = 1;
+					}
+					// otherwise use parseint
+					else {
+						exp[i] = Integer.parseInt(values[1]);
+					}
+				}
+			}
+		} catch (FileNotFoundException e) {
+			System.err.println("File not found: " + e.getMessage());
+		}
 	}
-	
+
 	//methods
 	public Polynomial add(Polynomial other) {
-		int maxlength = Math.max(coe.length, other.coe.length);
-
-		// create an array for storing the resulting coefficients after adding the two polynomials
-		// note that the array should have the same length as the longer polynomial
-		double[] result = new double[maxlength];
-
-		// use for loop to populate the array
-		for(int i = 0; i < maxlength; i++) {
-			double this_coe;
-			double other_coe;
-
-			// check if the index is out of bound for the current polynomial
-			if (i >= coe.length)
-				this_coe = 0;
-			else
-				this_coe = coe[i];
-
-			// check if the index is out of bound for the other polynomial
-			if (i >= other.coe.length)
-				other_coe = 0;
-			else
-				other_coe = other.coe[i];
-
-			// add the processed coefficients from both polynomial
-			result[i] = this_coe + other_coe;
+		// handle the case when the other polynomial has null coe (possibly constructed using the no-argument constructor)
+		if(other.coe == null) {
+			return new Polynomial(coe, exp);
 		}
 
+		// use a hashmap where keys are the exponent and the values
+		// are the coefficients
+		HashMap<Integer, Double> result = new HashMap<>();
+
+		// use for loop to loop through the coefficients and exponents for this polynomial
+		for(int i = 0; i < coe.length; i++) {
+			double this_coe = coe[i];
+			int this_exp = exp[i];
+			
+			// add the coefficient from this polynomial to the hashmap
+			// under the corresponding key (exponent)
+			if (result.containsKey(this_exp)) { 
+				result.put(this_exp, result.get(this_exp) + this_coe);
+			}
+			else {
+				result.put(this_exp, this_coe);
+			}
+		}
+		
+		// use for loop to loop through the coefficients and exponents for the other polynomial
+		// we use separate loop for the polynomials to take into consideration of the difference in length
+		for(int j = 0; j < other.coe.length; j++) {
+			double other_coe = other.coe[j];
+			int other_exp = other.exp[j];
+
+			// add the coefficient from the other polynomial to the hashmap
+			// under the corresponding key (exponent)
+			if (result.containsKey(other_exp)) {
+				result.put(other_exp, result.get(other_exp) + other_coe);
+			}
+			else {
+				result.put(other_exp, other_coe);
+			}
+		}
+
+		// count the number of non-zero coefficients and set that count as the size for the arrays
+        int size = 0;
+        for (Map.Entry<Integer, Double> entry : result.entrySet()) {
+            if (entry.getValue() != 0.0) {
+                size++;
+            }
+        }
+
+		// create an array of exponents and coefficients from the key-value pairs from the hashmap
+		int[] result_exp = new int[size];
+		double[] result_coe = new double[size];
+		int i = 0;
+		for (Map.Entry<Integer,Double> entry : result.entrySet()) {
+			// only if the coefficient is not 0 do we put this pair into the corresponding arrays
+			if(entry.getValue() != 0.0) {
+				result_exp[i] = entry.getKey();
+				result_coe[i] = entry.getValue();
+				i++;
+			}
+		}
 
 		// return the new Polynomial
-		return new Polynomial(result);
+		return new Polynomial(result_coe, result_exp);
 	}
 	
 	public double evaluate(double x) {
+		// handle the case when coe is null
+		if(coe == null) {
+			return 0.0;
+		}
+		
 		// create an double variable to store the evaluation result
 		double result = 0;
 		// use for loop to multiply the coefficient by the x value raised to the corresponding exponent
 		for(int i = 0; i < coe.length; i++)
-			result += coe[i] * Math.pow(x, i);
+			result += coe[i] * Math.pow(x, exp[i]);
 		
 		// return the evaluation result
 		return result;
@@ -68,5 +174,94 @@ public class Polynomial {
 			return true;
 		// otherwise return false
 		return false;
+	}
+
+	public Polynomial multiply(Polynomial other) {
+		// handle the case when the other polynomial has null coe (possibly constructed using the no-argument constructor)
+		// if the other polynomial has null coe, it means it's 0. so mulitplying any polynomial by 0 gives 0
+		if(other.coe == null) {
+			return new Polynomial();
+		}
+
+		// use a hashmap where keys are the exponent and the values
+		// are the coefficients
+		HashMap<Integer, Double> result = new HashMap<>();
+
+		// use double for loop to go through every coefficients in both polynomials
+		for(int i = 0; i < coe.length; i++) {
+			// now for each term in this polynomial, we mulitply it to every terms in the other polynomial
+			for(int j = 0; j < other.coe.length; j++) {
+				double coefficient = coe[i] * other.coe[j]; 
+				int exponent = exp[i] + other.exp[j];
+
+				// add the resulting coefficient to the hashmap
+				// under the resulting key (exponent)
+				if (result.containsKey(exponent)) { 
+					result.put(exponent, result.get(exponent) + coefficient);
+				}
+				else {
+					result.put(exponent, coefficient);
+				}
+			}
+		}
+		
+		// count the number of non-zero coefficients and set that count as the size for the arrays
+        int size = 0;
+        for (Map.Entry<Integer, Double> entry : result.entrySet()) {
+            if (entry.getValue() != 0.0) {
+                size++;
+            }
+        }
+
+		// create an array of exponents and coefficients from the key-value pairs from the hashmap
+		int[] result_exp = new int[size];
+		double[] result_coe = new double[size];
+		int i = 0;
+		for (Map.Entry<Integer,Double> entry : result.entrySet()) {
+			// only if the coefficient is not 0 do we put this pair into the corresponding arrays
+			if(entry.getValue() != 0.0) {
+				result_exp[i] = entry.getKey();
+				result_coe[i] = entry.getValue();
+				i++;
+			}
+		}
+
+		// return the new Polynomial
+		return new Polynomial(result_coe, result_exp);
+	}
+
+	public void saveToFile(String filename) {
+		try (PrintStream ps = new PrintStream(filename)) {
+			// handle the case where coe is null
+			if(coe == null) {
+				ps.print("0");
+				return;
+			}
+
+			// loop through all the coefficients (exponents) of this polynomial
+			for(int i = 0; i < coe.length; i++) {
+				double coefficient = coe[i];
+				int exponent = exp[i];
+
+				// if it's not the first term in the line and the coefficient is positive, we need to add + before it
+				if(i > 0 && coefficient > 0) {
+					// use print so that we are writing on the same line
+					ps.print("+");
+				}
+
+				// for the first term and negative coefficients, just print direcly
+				if(exponent == 0) {
+					ps.print(coefficient);
+				}
+				else if(exponent == 1) {
+					ps.print(coefficient + "x");
+				}
+				else {
+					ps.print(coefficient + "x" + exponent);
+				}
+			}	
+		} catch (FileNotFoundException e) {
+			System.err.println("File not found: " + e.getMessage());
+		}
 	}
 }
